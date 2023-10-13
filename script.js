fc48ed154157251142a47aa97c714a49b99ecd43
devjunior30 <edyjunior299@gmail.com>
2023-10-12 21:00:08 -0300

    nova pasta air max

---
+++ script.js
@@ -0,0 +1,22 @@
+let trocarImagem = document.querySelector("#mainPhoto")
+
+
+function troquei(endereco) {
+    
+    trocarImagem.src = endereco
+
+}
+
+let meuMenu = document.querySelectorAll(".links")
+
+meuMenu.style.maxHeight = "0px"
+
+function showMenu() {
+
+    if(meuMenu.style.maxHeight == "0px") {
+        meuMenu.style.maxHeight = "120px"
+    } else {
+        meuMenu.style.maxHeight = "0px"
+    }
+
+}
