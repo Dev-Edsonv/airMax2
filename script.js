42bcddee1afc628ef4f0fbebbf41b3b39172a2f9
devjunior30 <106643965+Edsonjj17@users.noreply.github.com>
2023-10-14 14:39:11 -0300

    ajustes no script e na estilização do site

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ function troquei(endereco) {
 }
 
 let meuMenu = document.querySelector(".links")
+let background =document.querySelector(".menu")
 
 meuMenu.style.maxHeight = "0px"
 
 function showMenu() {
 
     if(meuMenu.style.maxHeight === "0px") {
-        meuMenu.style.maxHeight = "120px"
+        meuMenu.style.maxHeight = "97px"
+        background.style.backgroundColor = "red"
     } else {
         meuMenu.style.maxHeight = "0px"
+        background.style.backgroundColor = "greenyellow"
     }
 
 }
